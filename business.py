c8a7d576e39276010a8a30edba0a25525cbcfc02
Jefferson Campos <jecampos@sorocaba.sp.gov.br>
2025-05-27 13:27:00 -0300

    feat: Course 01.
    Full course. Plus print and list_by.

--- business.py
+++ business.py
@@ -109,3 +109,56 @@ def get_initial_catalog() -> List[Game]:
         Game(name='Zelda\'s Majora\'s Mask', category=GameCategory.ADVENTURE, console=GameConsole.N64),
         Game(name='Zelda Ocarina of Time', category=GameCategory.ADVENTURE, console=GameConsole.N64)
     ]
+
+
+class User:
+    def __init__(self, name: str, username: str, password: str) -> None:
+        self._name: str = name
+        self._username: str = username
+        self._password: str = password
+
+    @property
+    def name(self) -> str:
+        return self._name
+
+    @name.setter
+    def name(self, vl: str) -> None:
+        self._name = vl
+
+    @property
+    def username(self) -> str:
+        return self._username
+
+    @username.setter
+    def username(self, vl: str) -> None:
+        self._username = vl
+
+    @property
+    def password(self) -> str:
+        return self._password
+
+    @password.setter
+    def password(self, vl) -> None:
+        self._password = vl
+
+    def __str__(self) -> str:
+        return f'[{self._username}] {self._name}'
+
+    def __repr__(self) -> str:
+        return f'[{self._username}] {self._name} :: {self._password}'
+
+    def authenticate(self, username: str, password: str) -> bool:
+        if self._username == username and self._password == password:
+            return True
+
+        return False
+
+
+def get_users() -> Dict[str, User]:
+    user_01: User = User(name='Jonh Doe', username='jonh_doe', password='A12345678a')
+    user_02: User = User(name='Jane Doe', username='jane_doe', password='a87654321A')
+
+    return {
+        user_01.username: user_01,
+        user_02.username: user_02
+    }
--- main.py
+++ main.py
@@ -1,11 +1,12 @@
 import os
-from typing import List
+from typing import Dict, List
 
 from dotenv import load_dotenv
-from flask import Flask, flash, redirect, render_template, request, session, url_for
+from flask import Flask, flash, redirect, render_template, request, send_file, session, url_for
 from termcolor import colored
+import weasyprint
 
-from business import Game, GameConsole, GameCategory, get_initial_catalog
+from business import Game, GameConsole, GameCategory, get_initial_catalog, get_users, User
 
 
 load_dotenv()
@@ -16,7 +17,9 @@ if DEBUG:
     print(colored(f'{ALLOWED_HOST=} :: {ALLOWED_PORT=}', 'yellow'))
 
 app = Flask(__name__)
+app.secret_key = str(os.getenv('SECRET_KEY'))
 game_catalog: List[str] = get_initial_catalog()
+users: Dict[str, User] = get_users()
 
 
 @app.route('/home')
@@ -46,7 +49,10 @@ def list_by_category():
 
 @app.route('/new')
 def new():
-    return render_template('new.html', title='New Game Setup')
+    if 'user' not in session or session['user'] is None:
+        return redirect(url_for('login', next_page=url_for('new')))
+
+    return render_template('new.html', title='New Game Setup', categories=[(e.name, e.value) for e in GameCategory], consoles=[(e.name, e.value) for e in GameConsole])
 
 
 @app.route('/create', methods=['POST',])
@@ -58,6 +64,52 @@ def create():
     game_catalog.append(Game(name=name, category=category, console=console))
 
     return redirect(url_for('list_games'))
+
+
+@app.route('/login')
+def login():
+    assert request.args.get('next_page')
+    return render_template('user/login.html', next_page=request.args.get('next_page'))
+
+
+@app.route('/logout')
+def logout():
+    session['user'] = None
+    flash('Logout was successfully')
+
+    return redirect(url_for('index'))
+
+
+@app.route('/authentication', methods=['POST',])
+def authentication():
+    if request.form['username'] in users:
+        user: User = users[request.form['username']]
+        if user.authenticate(username=request.form['username'], password=request.form['password']):
+            session['user'] = user.username
+            flash(f'{user} was successfully logged!!')
+
+            # return redirect(url_for(request.form['next_page']))
+            return redirect(request.form['next_page'])
+        else:
+            session['user'] = None
+            flash(f'{user} failed to login!!')
+
+            return redirect(url_for('login', next_page=request.form['next_page']))
+
+    else:
+        session['user'] = None
+        flash(f'{request.form["username"]} User not available!!')
+
+        return redirect(url_for('login', next_page=request.form['next_page']))
+
+
+@app.route('/print')
+def print():
+    css = weasyprint.CSS('static/print.css')
+    html = weasyprint.HTML(string=render_template('list_print.html', title='Printable Game List', games=game_catalog))
+    html.write_pdf('/tmp/game_list.pdf', stylesheets=[css])
+
+    return send_file('/tmp/game_list.pdf', download_name='game_list.pdf', mimetype='application/pdf')
 
 
 app.run(host=ALLOWED_HOST, port=ALLOWED_PORT)
